17f3f1283dccccd258e412b756c0d7aca21c5451
git-krishen <117951078+git-krishen@users.noreply.github.com>
2024-02-26 15:15:10 -0600

    Added multithreading

--- webcamtest.py
+++ webcamtest.py
@@ -5,9 +5,11 @@ from wpimath.geometry import Pose3d
 import numpy
 from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer, CvSink, CvSource, VideoMode
 from ntcore import NetworkTableInstance, GenericPublisher, GenericEntry, EventFlags, Topic, PubSubOptions
+import multiprocessing
+from time import sleep
 
 team = 5052
-server = False
+server = True
 xRes = 320
 yRes = 224
 DETECTION_MARGIN_THRESHOLD = 100
@@ -49,15 +51,53 @@ def detect_and_process_apriltag(frame, detector, estimator):
 
     return frame, results
 
+def initNetworkTable():
+    ntinst = NetworkTableInstance.getDefault()
+    if server:
+        print("Setting up NetworkTables server")
+        ntinst.startServer()
+        table = ntinst.getTable("apriltag")
+        tagIdEntry = table.getEntry("tagID")
+        translationEntry = table.getEntry("translation")
+        rotationEntry = table.getEntry("rotation")
+        centerEntry = table.getEntry("center")
+        idQueryEntry = table.getEntry("idQuery")
+        foundEntry = table.getEntry("found")
+    else:
+        print("Setting up NetworkTables client for team {}".format(team))
+        ntinst.startClient4("wpilibpi")
+        ntinst.setServerTeam(team)
+        ntinst.startDSClient()
+        table = ntinst.getTable("apriltag")
+        tagIdEntry = table.getEntry("tagID")
+        translationEntry = table.getEntry("translation")
+        rotationEntry = table.getEntry("rotation")
+        centerEntry = table.getEntry("center")
+        idQueryEntry = table.getEntry("idQuery")
+        foundEntry = table.getEntry("found")
+    idQueryEntry.setInteger(idQueryEntry.getInteger(0))
+    
+    return tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry
+
+def initCaptureAndEstimator():
+    cvSink, outputStream = startCamera()
+    image = numpy.zeros((xRes, yRes, 3), dtype = "uint8")
+    detector, estimator = get_apriltag_detector_and_estimator((xRes, yRes))
+    return cvSink, detector, estimator, image, outputStream
+
 # Display loop: captures a frame, detects apriltags, puts frame in outputstream, and publishes pose data to networktables
-def show_capture(cvSink, detector, estimator, image, outputstream, tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry, table):
+def show_capture():
+    # start NetworkTables
+    tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry = initNetworkTable()
+    cvSink, detector, estimator, image, outputStream = initCaptureAndEstimator()
     while True:
+        print("executed")
         # Capture frame-by-frame
         time, frame = cvSink.grabFrame(image)
         # Detect apriltag
         frame_with_maybe_apriltags, results = detect_and_process_apriltag(frame, detector, estimator)
 
-        outputstream.putFrame(frame_with_maybe_apriltags)
+        outputStream.putFrame(frame_with_maybe_apriltags)
 
         """
         Check for an entry called "idQuery"
@@ -100,10 +140,10 @@ def show_capture(cvSink, detector, estimator, image, outputstream, tagIdEntry, t
 
 
 def startCamera():
-    # Start running the camera.
+    """Start running the camera."""
     print("Starting camera '{}' on {}".format("Webcam", "/dev/video0"))
     # camera = UsbCamera("Webcam", "/dev/video0")
-    camera = CameraServer.startAutomaticCapture()
+    camera = CameraServer.startAutomaticCapture(1)
     camera.setResolution(xRes, yRes)
     # camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kConnectionKeepOpen)
     cvSink = CameraServer.getVideo()
@@ -111,33 +151,22 @@ def startCamera():
     outputStream = CameraServer.putVideo("WebcamVid", xRes, yRes)
     return cvSink, outputStream
 
+def runPixy():
+    #TODO begin implementation of Pixy code
+    while True:
+        sleep(1)
+        print("Process 2")
+
 # Main function:
 # Initializes capture & display window, initializes Apriltag detection, shows capture, cleans up
 def main():
-    # start NetworkTables
-    ntinst = NetworkTableInstance.getDefault()
-    if server:
-        print("Setting up NetworkTables server")
-        ntinst.startServer()
-    else:
-        print("Setting up NetworkTables client for team {}".format(team))
-        ntinst.startClient4("wpilibpi")
-        ntinst.setServerTeam(team)
-        ntinst.startDSClient()
-        
-    table = ntinst.getTable("apriltag")
-    tagIdEntry = table.getEntry("tagID")
-    translationEntry = table.getEntry("translation")
-    rotationEntry = table.getEntry("rotation")
-    centerEntry = table.getEntry("center")
-    idQueryEntry = table.getEntry("idQuery")
-    foundEntry = table.getEntry("found")
     
-    idQueryEntry.setInteger(idQueryEntry.getInteger(0))
-    cvSink, outputStream = startCamera()
-    image = numpy.zeros((xRes, yRes, 3), dtype = "uint8")
-    detector, estimator = get_apriltag_detector_and_estimator((xRes, yRes))
-    show_capture(cvSink, detector, estimator, image, outputStream, tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry, table)
+    first = multiprocessing.Process(target=show_capture)
+    second = multiprocessing.Process(target=runPixy)
+    first.start()
+    second.start()
+    first.join()
+    second.join()
 
 if __name__ == '__main__':
     main()
