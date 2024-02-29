12060a74fc3960b1e244fb7080d09fd1f60e0cbb
git-krishen <117951078+git-krishen@users.noreply.github.com>
2024-02-28 23:38:28 -0600

    Second webcam ring detection (not finished)
    To do:
    -Test thresholding and detection
    -Look into OpenCV camera calibration
    -Use detected ring to return required rotation

--- webcamtest.py
+++ webcamtest.py
@@ -1,10 +1,10 @@
 # Credit: Lobrien
 import cv2
 import robotpy_apriltag
-from wpimath.geometry import Pose3d
 import numpy
-from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer, CvSink, CvSource, VideoMode
-from ntcore import NetworkTableInstance, GenericPublisher, GenericEntry, EventFlags, Topic, PubSubOptions
+from wpimath.geometry import Pose3d
+from cscore import CameraServer
+from ntcore import NetworkTableInstance
 import multiprocessing
 from time import sleep
 
@@ -56,31 +56,27 @@ def initNetworkTable():
     if server:
         print("Setting up NetworkTables server")
         ntinst.startServer()
-        table = ntinst.getTable("apriltag")
-        tagIdEntry = table.getEntry("tagID")
-        translationEntry = table.getEntry("translation")
-        rotationEntry = table.getEntry("rotation")
-        centerEntry = table.getEntry("center")
-        idQueryEntry = table.getEntry("idQuery")
-        foundEntry = table.getEntry("found")
+        
     else:
         print("Setting up NetworkTables client for team {}".format(team))
         ntinst.startClient4("wpilibpi")
         ntinst.setServerTeam(team)
         ntinst.startDSClient()
-        table = ntinst.getTable("apriltag")
-        tagIdEntry = table.getEntry("tagID")
-        translationEntry = table.getEntry("translation")
-        rotationEntry = table.getEntry("rotation")
-        centerEntry = table.getEntry("center")
-        idQueryEntry = table.getEntry("idQuery")
-        foundEntry = table.getEntry("found")
+        
+    table = ntinst.getTable("apriltag")
+    tagIdEntry = table.getEntry("tagID")
+    translationEntry = table.getEntry("translation")
+    rotationEntry = table.getEntry("rotation")
+    centerEntry = table.getEntry("center")
+    idQueryEntry = table.getEntry("idQuery")
+    foundEntry = table.getEntry("found")
+    foundTagsEntry = table.getEntry("foundTags")
     idQueryEntry.setInteger(idQueryEntry.getInteger(0))
     
-    return tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry
+    return tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry, foundTagsEntry
 
 def initCaptureAndEstimator():
-    cvSink, outputStream = startCamera()
+    cvSink, outputStream = startCamera(0)
     image = numpy.zeros((xRes, yRes, 3), dtype = "uint8")
     detector, estimator = get_apriltag_detector_and_estimator((xRes, yRes))
     return cvSink, detector, estimator, image, outputStream
@@ -88,7 +84,7 @@ def initCaptureAndEstimator():
 # Display loop: captures a frame, detects apriltags, puts frame in outputstream, and publishes pose data to networktables
 def show_capture():
     # start NetworkTables
-    tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry = initNetworkTable()
+    tagIdEntry, translationEntry, rotationEntry, centerEntry, idQueryEntry, foundEntry, foundTagsEntry = initNetworkTable()
     cvSink, detector, estimator, image, outputStream = initCaptureAndEstimator()
     while True:
         print("executed")
@@ -112,6 +108,10 @@ def show_capture():
         translationEntry.setDoubleArray([0, 0, 0]) 
         rotationEntry.setDoubleArray([0, 0, 0])
         centerEntry.setDoubleArray([0, 0])
+        foundTags = []
+        for result in results :
+            foundTags.append(result)
+        foundTagsEntry.setIntegerArray(foundTags)
         foundEntry.setBoolean(False)
         if(idQuery > 0) :
             for result in results :
@@ -139,11 +139,11 @@ def show_capture():
                 foundEntry.setBoolean(True)
 
 
-def startCamera():
+def startCamera(cameraPort):
     """Start running the camera."""
     print("Starting camera '{}' on {}".format("Webcam", "/dev/video0"))
     # camera = UsbCamera("Webcam", "/dev/video0")
-    camera = CameraServer.startAutomaticCapture(1)
+    camera = CameraServer.startAutomaticCapture(cameraPort)
     camera.setResolution(xRes, yRes)
     # camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kConnectionKeepOpen)
     cvSink = CameraServer.getVideo()
@@ -151,21 +151,53 @@ def startCamera():
     outputStream = CameraServer.putVideo("WebcamVid", xRes, yRes)
     return cvSink, outputStream
 
-def runPixy():
-    #TODO begin implementation of Pixy code
+def runRingDetector():
+    cvSink, outputStream = startCamera(1)
+    image = numpy.zeros((xRes, yRes, 3), dtype = "uint8")
+    ORANGE_MIN = numpy.array([1, 95, 150])
+    ORANGE_MAX = numpy.array([27, 255, 255])
     while True:
-        sleep(1)
-        print("Process 2")
+        print("second func")
+        time, frame = cvSink .grabFrame(image)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(frame, ORANGE_MIN, ORANGE_MAX)
+        frame = cv2.bitwise_and(frame, frame, mask=mask)
+        frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        big_ellipse = None
+        max_area = 0
+        for contour in contours:
+            if len(contour) >= 5:
+                ellipse = cv2.fitEllipse(contour)
+                width = ellipse[1][0]
+                height = ellipse[1][1]
+                area = width * height * numpy.pi # Calculate the area of the fitted ellipse
+                if area > max_area and width > 10 and height > 10:
+                    max_area = area
+                    big_ellipse = ellipse
+
+
+        
+        cv2.ellipse(frame, big_ellipse, (0,255,0), 2,2)
+        cv2.imshow('final', frame)
+        cv2.waitKey(1)
+
+        #TODO figure out OpenCV camera calibration
+        #TODO implement matrix to find angle to camera of object
+
+
 
 # Main function:
 # Initializes capture & display window, initializes Apriltag detection, shows capture, cleans up
 def main():
     
     first = multiprocessing.Process(target=show_capture)
-    second = multiprocessing.Process(target=runPixy)
-    first.start()
+    second = multiprocessing.Process(target=runRingDetector)
+    # first.start()
     second.start()
-    first.join()
+    # first.join()
     second.join()
 
 if __name__ == '__main__':
